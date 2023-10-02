943d6e9672f14f8f6f8bb38b73796da66d4ed047
alcabarcasb@gmail.com <alcabarcasb@gmail.com>
2023-10-02 14:34:46 -0500

    Login commit

--- react-native-walkthrough-master/src/screens/WalkthroughScreen/Login.js
+++ react-native-walkthrough-master/src/screens/WalkthroughScreen/Login.js
@@ -1,9 +1,7 @@
 import React, { useState } from 'react';
 import { View, TextInput, Button, Alert } from 'react-native';
-import Estilos from "./styles";
-//import Homescreen from './HomeScreen';
 
-const styles = Estilos(appStyles, colorScheme);
+
 const LoginScreen = ({ navigation }) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
@@ -21,7 +19,7 @@ const LoginScreen = ({ navigation }) => {
     <View>
       <TextInput placeholder="Usuario" onChangeText={setUsername} />
       <TextInput placeholder="Contraseña" secureTextEntry onChangeText={setPassword} />
-      <Button title="Iniciar sesión" onPress={() => this.props.navigation.navigate('HomeScreen')} />
+      <Button title="Iniciar sesión" onPress={handleLogin} />
     </View>
   );
 };
